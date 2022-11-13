414313175946e64ab0cd82c5a3a580a96ad855ee
gorozhin <m.gorozhin@gmail.com>
2022-11-13 16:28:39 +0300

    Change strings to chars

--- src/positioning.lisp
+++ src/positioning.lisp
@@ -30,16 +30,16 @@
                                                                   null)
                                                         ,function-name))
                                         (defun ,function-name (&key (n 1) (stream *standard-output*))
-                                          (format stream "~C[~A~A" #\Esc n ,control-sequence))
+                                          (format stream "~C[~A~C" #\Esc n ,control-sequence))
                                         (setf (documentation ',function-name 'function)
                                               ,documentation)
                                         (export ',function-name '#:chlorophyll))))))
   
   (define-cursor-positioning-functions
-    cursor-up            "A" "Moves cursor N rows up"
-    cursor-down          "B" "Moves cursor N rows down"
-    cursor-forward       "C" "Moves cursor N columns forward"
-    cursor-backward      "D" "Moves cursor N columns backward"
-    cursor-next-line     "E" "Moves cursor to beginning of the line n lines up"
-    cursor-previous-line "F" "Moves cursor to beginning of the line n lines down"))
+    cursor-up            #\A "Moves cursor N rows up"
+    cursor-down          #\B "Moves cursor N rows down"
+    cursor-forward       #\C "Moves cursor N columns forward"
+    cursor-backward      #\D "Moves cursor N columns backward"
+    cursor-next-line     #\E "Moves cursor to beginning of the line n lines up"
+    cursor-previous-line #\F "Moves cursor to beginning of the line n lines down"))
 
